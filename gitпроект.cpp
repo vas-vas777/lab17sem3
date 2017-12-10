d3a12955d4e4fa79695df5ee5cf2027de5cbca91
vas-vas777 <vasya1998@list.ru>
2017-12-10 09:12:59 +0300

    Add files via upload

---
+++ gitпроект.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<conio.h>
+
+int main()
+{
+	int a, b, result;
+	
+	printf("a= "); scanf_s("%d", &a);
+	printf("b= "); scanf_s("%d", &b);
+	printf("result of div= "); result = a / b;
+	_getch();
+	return 0;
+}
+
